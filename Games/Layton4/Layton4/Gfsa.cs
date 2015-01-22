ad7478eb21417cc86ef83c7a0bf6a0b6bfb38992
Benito Palacios Sánchez <benito356@gmail.com>
2015-01-22 14:18:58 +0100

    Implement header block exporter

---
+++ Games/Layton4/Layton4/Gfsa.cs
@@ -0,0 +1,73 @@
+﻿//
+//  Gfsa.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Libgame;
+using Libgame.IO;
+using Mono.Addins;
+
+namespace Layton4
+{
+	[Extension]
+	public class Gfsa : Format
+	{
+		const string MagicStamp = "GFSA";
+		const int NumHeaders = 4;
+
+		public override string FormatName {
+			get { return "Laytn4.Gfsa"; }
+		}
+
+		public override void Read(DataStream strIn)
+		{
+			var reader = new DataReader(strIn);
+			if (reader.ReadString(4) != MagicStamp)
+				throw new FormatException();
+
+			for (int i = 0; i < NumHeaders; i++) {
+				uint offset = reader.ReadUInt32();
+				uint nextOffset = reader.ReadUInt32(); strIn.Seek(-4, SeekMode.Current);
+
+				var subStream = new DataStream(strIn, offset, nextOffset - offset);
+				this.File.AddFile(new GameFile("Block" + i.ToString() + ".bin", subStream));
+			}
+		}
+
+		public override void Write(DataStream strOut)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Import(params DataStream[] strIn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Export(params DataStream[] strOut)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void Dispose(bool freeManagedResourcesAlso)
+		{
+		}
+	}
+}
+
--- Games/Layton4/Layton4/Program.cs
+++ Games/Layton4/Layton4/Program.cs
@@ -19,6 +19,10 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Libgame;
 
 namespace Layton4
 {
@@ -26,6 +30,21 @@ namespace Layton4
 	{
 		public static void Main(string[] args)
 		{
+			if (args.Length < 1)
+				return;
+
+			string executableDir = args[0];
+			var root = GameFolderFactory.FromPath(executableDir);
+			FileManager.Initialize(root, new FileInfoCollection());
+
+			var container = root.Files["lt4_main_sp.fa"] as GameFile;
+			if (container == null)
+				return;
+
+			container.SetFormat<Gfsa>();
+			container.Format.Read();
+			foreach (var block in container.Files.Cast<GameFile>())
+				block.Stream.WriteTo(Path.Combine(executableDir, block.Name));
 		}
 	}
 }
