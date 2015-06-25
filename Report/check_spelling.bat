ae8b7e7b550b3bdc223d8499d28a3ec179db43b1
Benito Palacios <benito356@gmail.com>
2015-06-25 03:46:14 +0200

    Add check_spelling for windows

---
+++ Report/check_spelling.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Aspell\bin\aspell.exe" -t --lang=es --encoding=ISO8859-1 --add-tex-command="includefigure pPp" --add-tex-command="ctable oppp" --personal=.\.aspell.es.pws check "%1"
