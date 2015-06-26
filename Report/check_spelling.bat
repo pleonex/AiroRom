389c178973093e97bb90db9b21567b182dcace75
Benito Palacios <benito356@gmail.com>
2015-06-27 01:38:14 +0200

    Fix spelling scripts and typos

--- Report/check_spelling.bat
+++ Report/check_spelling.bat
@@ -1,1 +1,3 @@
-"C:\Program Files (x86)\Aspell\bin\aspell.exe" -t --lang=es --encoding=ISO8859-1 --add-tex-command="includefigure pPp" --add-tex-command="ctable oppp" --personal=.\.aspell.es.pws check "%1"
+@echo off
+chcp 28591
+"C:\Program Files (x86)\Aspell\bin\aspell.exe" -t --lang=es --encoding=ISO8859-1 --add-tex-command="includefigure pPp" --add-tex-command="ctable oppp" --add-tex-command="textit p" --add-tex-command="acl p" --add-tex-command="ac p" --add-tex-command="acs p" --add-tex-command="texttt p" --personal="%~p0.aspell.es.pws" check "%1"
--- Report/check_spelling.sh
+++ Report/check_spelling.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aspell -t --lang=es --encoding=ISO-8859-1 --add-tex-command="includefigure pPp" --add-tex-command="ctable oppp" --personal=./.aspell.es.pws check "$1"
+aspell -t --lang=es --encoding=ISO-8859-1 --add-tex-command="includefigure pPp" --add-tex-command="ctable oppp" --add-tex-command="textit p" --add-tex-command="acl p" --add-tex-command="ac p" --add-tex-command="acs p" --add-tex-command="texttt p" --personal=./.aspell.es.pws check "$1"
